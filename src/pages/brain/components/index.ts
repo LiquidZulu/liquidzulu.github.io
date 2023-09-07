4657139ad9b8efa76ba12bf7980e0e52737baa5d
LiquidZulu <liquidzulu@pm.me>
2023-09-07 15:53:57 +0100

    alter style of fuzzy search

--- src/pages/brain/components/index.ts
+++ src/pages/brain/components/index.ts
@@ -1,3 +1,4 @@
 export { default as BrainNavbar } from './BrainNavbar.astro';
 export { default as BrainPanel } from './BrainPanel.html';
 export { default as BrainTag } from './BrainTag.astro';
+export { default as FuzzySearch } from './FuzzySearch.astro';
