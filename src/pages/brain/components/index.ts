21b530ae32012b807972f2beaa873e5e447ec1b1
LiquidZulu <liquidzulu@pm.me>
2023-09-07 14:14:07 +0100

    maybe it wants me to use html for some ungodly reason? ¯\_(ツ)_/¯

--- src/pages/brain/components/index.ts
+++ src/pages/brain/components/index.ts
@@ -1,3 +1,3 @@
 export { default as BrainNavbar } from './BrainNavbar.astro';
-export { default as BrainPanel } from './BrainPanel.astro';
+export { default as BrainPanel } from './BrainPanel.html';
 export { default as BrainTag } from './BrainTag.astro';
