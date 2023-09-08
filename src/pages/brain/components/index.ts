87f4baeb1f09f93bac626526375e7e821192703f
LiquidZulu <liquidzulu@pm.me>
2023-09-08 01:01:04 +0100

    brain divider

--- src/pages/brain/components/index.ts
+++ src/pages/brain/components/index.ts
@@ -1,3 +1,4 @@
+export { default as BrainDivider } from './BrainDivider.html';
 export { default as BrainNavbar } from './BrainNavbar.astro';
 export { default as BrainPanel } from './BrainPanel.html';
 export { default as BrainTag } from './BrainTag.astro';
