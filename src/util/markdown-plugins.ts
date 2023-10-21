7303c9d0480da3317d26d119cda1c78c78cd48fa
LiquidZulu <liquidzulu@pm.me>
2023-10-21 18:37:05 +0100

    abstract node replacer function

--- src/util/markdown-plugins.ts
+++ src/util/markdown-plugins.ts
@@ -1,37 +1,40 @@
 // note: for some reason spread syntax does not work here, so Object.assign is required
 
+type TNodeValue = { value: string; [key: string]: any };
+type NodeNodeMap = (node: TNodeValue) => TNodeValue;
+type NodeStringMap = (node: TNodeValue) => string;
 import { regexReplace } from './regexReplace';
 
-type TNodeValue = { value: string; [key: string]: any };
+const nodeReplacer: (
+    node: TNodeValue,
+    replacer: NodeStringMap
+) => TNodeValue = (node, replacer) =>
+    Object.assign(node, {
+        value: replacer(node),
+    });
 
-export const [leftArrow, rightArrow, upArrow, downArrow, leftRightArrow]: Array<
-    (node: TNodeValue) => TNodeValue
-> = [
+export const [
+    leftArrow,
+    rightArrow,
+    upArrow,
+    downArrow,
+    leftRightArrow,
+]: Array<NodeNodeMap> = [
     node =>
-        Object.assign(node, {
-            value: regexReplace(node.value, /<-/g, x => '←'),
-        }),
+        nodeReplacer(node, node => regexReplace(node.value, /<-/g, x => '←')),
     node =>
-        Object.assign(node, {
-            value: regexReplace(node.value, /->/g, x => '→'),
-        }),
+        nodeReplacer(node, node => regexReplace(node.value, /->/g, x => '→')),
     node =>
-        Object.assign(node, {
-            value: regexReplace(node.value, /\|\^/g, x => '↑'),
-        }),
+        nodeReplacer(node, node => regexReplace(node.value, /\|\^/g, x => '↑')),
     node =>
-        Object.assign(node, {
-            value: regexReplace(node.value, /\|v/g, x => '↓'),
-        }),
+        nodeReplacer(node, node => regexReplace(node.value, /\|v/g, x => '↓')),
     node =>
-        Object.assign(node, {
-            value: regexReplace(node.value, /<->/g, x => '↔'),
-        }),
+        nodeReplacer(node, node => regexReplace(node.value, /<->/g, x => '↔')),
 ];
 
 const arrowFns = [leftArrow, rightArrow, upArrow, downArrow, leftRightArrow];
 
-export const unicodeArrows = (node: TNodeValue) => {
+export const unicodeArrows: NodeNodeMap = node => {
     for (let arrowFn of arrowFns) {
         node = arrowFn(node);
     }
