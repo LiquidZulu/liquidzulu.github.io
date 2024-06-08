3a003743da5c872f59fa5ba0109106ef7e41cbda
LiquidZulu <liquidzulu@pm.me>
2024-06-08 21:57:08 +0100

    update tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,55 +7,34 @@ import remarkWikilink from '@portaljs/remark-wiki-link';
 import { visit } from 'unist-util-visit';
 import { readdir } from 'node:fs/promises';
 import { regexReplace } from './src/util/regexReplace';
-
-import {
-    unicodeArrows,
-    fixObsidianDashes,
-    obsidianWikilinks,
-    paragraphLinks,
-    obsidianReplaceEmptyTableRow,
-} from './src/util/markdown-plugins';
-import {
-    wikilinksToHypertextLinks,
-    wikilinksToMdLinks,
-} from './src/util/wikilinks';
+import { unicodeArrows, fixObsidianDashes, obsidianWikilinks, paragraphLinks, obsidianReplaceEmptyTableRow } from './src/util/markdown-plugins';
+import { wikilinksToHypertextLinks, wikilinksToMdLinks } from './src/util/wikilinks';
 import { getSlug } from './src/util/getSlug';
-
 const files = await readdir('./src/content/brain');
-const filesProc = files
-    .filter(file => !file.startsWith('.'))
-    .map(file => getSlug(file.split('.')[0]));
-
+const filesProc = files.filter(file => !file.startsWith('.')).map(file => getSlug(file.split('.')[0]));
 const isObsidian = file => file.path.match(/content\/brain/g) !== null; // | check_for_some_other_vault
 
+
+// https://astro.build/config
 export default defineConfig({
-    site: 'https://liquidzulu.github.io',
-    integrations: [tailwind(), prefetch(), icon(), markdoc()],
-    markdown: {
-        remarkPlugins: [
-            // mmmm, curry
-            () => (ast, file) => {
-                visit(ast, 'text', unicodeArrows);
-                if (isObsidian(file)) {
-                    visit(ast, 'text', fixObsidianDashes);
-                    visit(ast, 'text', obsidianWikilinks(filesProc));
-                    visit(
-                        ast,
-                        'tableRow',
-                        obsidianReplaceEmptyTableRow(emptyRow =>
-                            Object.assign(emptyRow, {
-                                type: 'html',
-                                value: `<tr style="box-shadow: inset 0 1px 0 0 #3f3f46; height: 1px;"></tr>`,
-                            })
-                        )
-                    );
-                }
-            },
-        ],
-        rehypePlugins: [
-            () => (ast, file) => {
-                visit(ast, 'element', paragraphLinks);
-            },
-        ],
-    },
+  site: 'https://liquidzulu.github.io',
+  integrations: [tailwind(), prefetch(), icon(), markdoc()],
+  markdown: {
+    remarkPlugins: [
+    // mmmm, curry
+    () => (ast, file) => {
+      visit(ast, 'text', unicodeArrows);
+      if (isObsidian(file)) {
+        visit(ast, 'text', fixObsidianDashes);
+        visit(ast, 'text', obsidianWikilinks(filesProc));
+        visit(ast, 'tableRow', obsidianReplaceEmptyTableRow(emptyRow => Object.assign(emptyRow, {
+          type: 'html',
+          value: `<tr style="box-shadow: inset 0 1px 0 0 #3f3f46; height: 1px;"></tr>`
+        })));
+      }
+    }],
+    rehypePlugins: [() => (ast, file) => {
+      visit(ast, 'element', paragraphLinks);
+    }]
+  }
 });
